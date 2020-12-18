c2a5a4229c2f07749800b21ec9a161bb3a1744fc
Andreas Steven <andreas.steven15@gmail.com>
2020-12-18 20:28:08 +0700

    Data Fix

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -15,6 +15,7 @@ export class DetailPage implements OnInit
 {
   datas: any = [];
   map: any;
+  TEMP: string;
   @ViewChild('ABC', {read: ElementRef, static: false}) ABC: ElementRef;
 
   sliderOpts = {
@@ -30,30 +31,47 @@ export class DetailPage implements OnInit
 
   ngOnInit() 
   { 
-    // this.activatedRoute.paramMap.subscribe(async paramMap => {
-    //   if (!paramMap.has('productId')) { return; }
-    //   const model = paramMap.get('productId');
 
-    //   console.log('Model: ', model);
-
-    //   await this.getData(model);
-      
-    // });
   }
 
   ngAfterViewInit() 
   {
-    this.showMap(this.umnPos);
-
     this.activatedRoute.paramMap.subscribe(async paramMap => {
       if (!paramMap.has('productId')) { return; }
       const model = paramMap.get('productId');
 
       console.log('Model: ', model);
 
+      if(model == "Avanza" || model == "Innova" || model == "Fortuner" || model == "Land Cruiser")
+      {
+        this.TEMP = "Toyota";
+      }
+      else if(model == "Xpander" || model == "Pajero")
+      {
+        this.TEMP = "Mitsubishi";
+      }
+      else if(model == "Brio" || model == "Civic")
+      {
+        this.TEMP = "Honda";
+      }
+
       await this.getData(model);
-      
     });
+
+    console.log("TEMP: ", this.TEMP);
+
+    if(this.TEMP == "Toyota")
+    {
+      this.showMap(this.toyotaPos);
+    }
+    else if(this.TEMP == "Mitsubishi")
+    {
+      this.showMap(this.mitsubishiPos);
+    }
+    else if(this.TEMP == "Honda")
+    {
+      this.showMap(this.hondaPos);
+    }
   }
 
   // openPreview()
@@ -98,6 +116,30 @@ export class DetailPage implements OnInit
     lng: 106.618755
   };
 
+  toyotaPos: any = 
+  {
+    lat: -6.246756214938007,
+    lng: 106.62689438050396
+  };
+
+  mitsubishiPos: any = 
+  {
+    lat: -6.275908734290353,
+    lng: 106.65846895396079
+  };
+
+  hondaPos: any = 
+  {
+    lat: -6.296393181483966,
+    lng: 106.66697866225027
+  };
+
+  suzukiPos: any = 
+  {
+    lat: -6.290311046708391,
+    lng: 106.78115963353491
+  };
+
   showMap(pos: any) 
   {
     const options = {
@@ -105,11 +147,29 @@ export class DetailPage implements OnInit
       zoom: 15,
       disableDefaultUI: true
     };
+
     this.map = new google.maps.Map(this.ABC.nativeElement, options);
-    
-    const marker = new google.maps.Marker({
-      position: this.umnPos,
-      map: this.map
-    });
+
+    if(this.TEMP == "Toyota")
+    {
+      const marker = new google.maps.Marker({
+        position: this.toyotaPos,
+        map: this.map
+      });
+    }
+    else if(this.TEMP == "Mitsubishi")
+    {
+      const marker = new google.maps.Marker({
+        position: this.mitsubishiPos,
+        map: this.map
+      });
+    }
+    else if(this.TEMP == "Honda")
+    {
+      const marker = new google.maps.Marker({
+        position: this.hondaPos,
+        map: this.map
+      });
+    }
   }
 }
